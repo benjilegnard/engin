9719a9dde63ac04974603fa73e32087860c44e1b
Benjamin Legrand <jibhaine@gmail.com>
2013-05-22 12:05:17 +0200

    correction of wrong file loaded

--- app/services.js
+++ app/services.js
@@ -1,15 +1,51 @@
-exports.users = function (req, res, next) {
-    res.json({'data':[]});
-};
-exports.repositories = function (req, res, next) {
-    res.json({'data':[]});
-};
-exports.rendition = function (req, res, next) {
-    res.json({'data':[]});
-};
-exports.folders = function (req, res, next) {
-    res.json({'data':[]});
-};
-exports.documents = function (req, res, next) {
-    res.json({'data':[]});
-};
+(function(exports){
+    'use strict';
+    var models = require('./models');
+
+    var CrudService = function(modelClass){
+        return {
+            "query":function(req,res,next){
+                modelClass.find(req.query)
+                    .success(function(){
+
+                    })
+                    .error(function(){
+
+                    })
+            },
+            "create":function(req,res,next){
+                modelClass.build(req.query)
+                    .success(function(){
+
+                    })
+                    .error(function(){
+
+                    })
+            },
+            "update":function(req,res,next){
+
+                modelClass.update(req.query)
+                    .success(function(){
+
+                    })
+                    .error(function(){
+
+                    })
+            },
+            "options":function(req,res,next){
+
+            },
+            "delete":function(req,res,next){
+
+            }
+        }
+    };
+
+    exports.users = CrudService(models.User);
+
+    exports.project = CrudService(models.Project);
+    exports.documents = CrudService(models.Document);
+    exports.rendition = CrudService(models.Rendition);
+
+
+})(module.exports);
